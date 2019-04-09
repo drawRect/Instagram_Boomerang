b7a7da7050e402a00892f41859a58093dfccfec6
Boominadha Prakash M <boominadha.pm@sonata-software.com>
2019-04-09 19:01:53 +0530

    Basic setup

---
+++ BoomerangVideo/BVCameraController.swift
@@ -0,0 +1,81 @@
+//
+//  BVCameraController.swift
+//  BoomerangVideo
+//
+//  Created by Sonata on 09/04/19.
+//  Copyright © 2019 DrawRect. All rights reserved.
+//
+
+import UIKit
+
+class BVCameraController: UIViewController {
+
+    let cameraView = BVCameraView(frame: UIScreen.main.bounds)
+    lazy var cameraManager: CameraManager = {
+        let cm = CameraManager()
+        cm.shouldEnableExposure = true
+        cm.shouldFlipFrontCameraImage = false
+        cm.showAccessPermissionPopupAutomatically = false
+        cm.cameraOutputQuality = CameraOutputQuality.high
+        cm.flashMode = CameraFlashMode.auto
+        return cm
+    }()
+    var statusBarShouldBeHidden = false
+    
+    override func loadView() {
+        view = cameraView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        cameraStatusCheck()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraManager.resumeCaptureSession()
+        statusBarShouldBeHidden = true
+        UIView.animate(withDuration: 0.25) {
+            self.setNeedsStatusBarAppearanceUpdate()
+        }
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        cameraManager.stopCaptureSession()
+    }
+    deinit {
+        if cameraManager.captureSession?.isRunning == true {
+            cameraManager.stopCaptureSession()
+        }
+    }
+    override var prefersStatusBarHidden: Bool {
+        return statusBarShouldBeHidden
+    }
+}
+
+extension BVCameraController {
+    func cameraStatusCheck() {
+        switch cameraManager.currentCameraStatus() {
+        case .notDetermined:
+            askCameraPermission()
+        case .ready:
+            addCamera()
+        default:
+            askCameraPermission()
+        }
+    }
+    func askCameraPermission() {
+        cameraManager.askUserForCameraPermission({ permissionGranted in
+            if permissionGranted {
+                self.addCamera()
+            } else {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(URL(string:UIApplication.openSettingsURLString)!)
+                } else {
+                    // Fallback on earlier versions
+                }
+            }
+        })
+    }
+    func addCamera() {
+        cameraManager.addPreviewLayerToView(cameraView.cameraView, newCameraOutputMode: CameraOutputMode.stillImage)
+    }
+}
---
+++ BoomerangVideo/BVCameraPreviewController.swift
@@ -0,0 +1,17 @@
+//
+//  BVCameraPreviewController.swift
+//  BoomerangVideo
+//
+//  Created by Sonata on 09/04/19.
+//  Copyright © 2019 DrawRect. All rights reserved.
+//
+
+import UIKit
+
+class BVCameraPreviewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
